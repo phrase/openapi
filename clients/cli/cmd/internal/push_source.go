d09224f7c1b15e9ce98a85fef78761697b48963d
Georgios Metaxakis <g.metaxakis@withplum.com>
2021-10-20 19:06:21 +0300

    Fix ignoring locale file tag

--- clients/cli/cmd/internal/push_source.go
+++ clients/cli/cmd/internal/push_source.go
@@ -183,8 +183,13 @@ func (source *Source) uploadFile(client *phrase.APIClient, localeFile *LocaleFil
 	}
         
 	if tag != "" {
-        params.Tags = optional.NewString(tag)
-    }
+		var v string
+		if params.Tags.IsSet() {
+			v = params.Tags.Value() + ","
+		}
+		v += tag
+		params.Tags = optional.NewString(v)
+    	}
 	
 	if branch != "" {
 		params.Branch = optional.NewString(branch)
