d0835ce2e3c471a5ac098b44148020beb264b6b7
Anthony Chang <anthony.chang@memsource.com>
2022-10-27 14:17:59 +0200

    use formatMessage helper instead to make cleaner code

--- clients/cli/cmd/internal/print/print.go
+++ clients/cli/cmd/internal/print/print.go
@@ -29,43 +29,40 @@ const phrase_logo = `
 `
 
 func PhraseLogo() {
-	WithColor(ct.Cyan, phrase_logo)
+	WithColor(ct.Cyan, "message", phrase_logo)
 }
 
 func Success(msg string, args ...interface{}) {
-	WithColor(ct.Green, msg, args...)
+	WithColor(ct.Green, "message", msg, args...)
 }
 
 func Failure(msg string, args ...interface{}) {
-	WithColor(ct.Red, msg, args...)
+	WithColor(ct.Red, "failure", msg, args...)
 }
 
-func WithColor(color ct.Color, msg string, args ...interface{}) {
-	fprintWithColor(os.Stdout, color, msg, args...)
+func WithColor(color ct.Color, mapKey string, msg string, args ...interface{}) {
+	fprintWithColor(os.Stdout, color, mapKey, msg, args...)
 }
 
 func Error(err error) {
-	fprintWithColor(os.Stderr, ct.Red, "ERROR: %s", err)
+	fprintWithColor(os.Stderr, ct.Red, "error", "ERROR: %s", err)
 }
 
-func fprintWithColor(w io.Writer, color ct.Color, msg string, args ...interface{}) {
+func fprintWithColor(w io.Writer, color ct.Color, mapKey string, msg string, args ...interface{}) {
 	ct.Foreground(color, true)
-	fmt.Fprintln(w, stringifyJsonMessage(msg, args...))
+	fmt.Fprintf(w, formatMessage(mapKey, msg, args...))
+	fmt.Fprintln(w)
 	ct.ResetColor()
 }
 
-// delegate printing to supplied print fn
-func PrintWithFn(f fn, msg string, args ...interface{}) {
+func formatMessage(mapKey string, msg string, args ...interface{}) {
+	formatted_string := fmt.Sprintf(msg, args...)
+
 	if Config.Json {
-		f(stringifyJsonMessage(msg, args...))
-	} else {
-		f(msg, args...)
+		msg_map := make(map[string]string)
+		msg_map[mapKey] = formatted_string
+		formatted_string, _ := string(json.Marshal(msg_map))
 	}
-}
 
-func stringifyJsonMessage(msg string, args ...interface{}) {
-	msg_string := fmt.Sprintf(msg, args...)
-	msg_json, _ := json.Marshal(msg_string)
-
-	return string(msg_json)
+	return formatted_string
 }
