ee141081c1ddc486642ca6d18b38f8332badcd86
Sönke Behrendt <soenke.behrendt@gmail.com>
2023-05-31 15:55:54 +0200

    CLI: Fix waiting for rate limit

--- clients/cli/cmd/internal/pull.go
+++ clients/cli/cmd/internal/pull.go
@@ -250,11 +250,22 @@ func (target *Target) createLocaleFiles(remoteLocale *phrase.Locale) (LocaleFile
 	return files, nil
 }
 
-func waitForRateLimit(response phrase.Rate) {
-	var reset time.Time
-	resetTime := reset.Add(time.Second * 5).Sub(time.Now())
-	fmt.Printf("Rate limit exceeded. Download will resume in %d seconds\n", int64(resetTime.Seconds()))
-	time.Sleep(resetTime)
+func waitForRateLimit(rate phrase.Rate) {
+	var duration = time.Until(rate.Reset.Time)
+	message := "\rRate limit exceeded. Download will resume in %d seconds"
+	seconds := int64(time.Until(rate.Reset.Time).Seconds())
+	fmt.Printf(message, seconds)
+	counter := int64(1)
+	ticker := time.NewTicker(1 * time.Second)
+	for range ticker.C {
+		counter++
+		seconds = int64(time.Until(rate.Reset.Time).Seconds())
+		fmt.Printf(message, seconds)
+		if counter > int64(duration/time.Second) {
+			ticker.Stop()
+			break
+		}
+	}
 }
 
 func createLocaleFile(target *Target, remoteLocale *phrase.Locale, tag string) (*LocaleFile, error) {
