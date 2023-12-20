7c04f1e1f9fd280f56e15520626166ab3b5b6db5
Hassan Ahmed <141136168+hahmed-dev@users.noreply.github.com>
2023-12-20 09:07:25 +0100

    fix(GO): Fix format_option serialization when creating upload (#498)
    * Conditionally handle nested params
    
    * moves type and properties keys under schema
    
    * remove example
    
    * remove example property altogether
    
    * removed deep object property (probably not supported from form body)
    
    * use custom serialization method
    
    * adds test cases
    
    * fix failing spec
    
    * update path for locale specs as well
    
    * fixed type

--- clients/go/test/api_locales_test.go
+++ clients/go/test/api_locales_test.go
@@ -16,7 +16,7 @@ import (
 	"testing"
 
 	"github.com/antihax/optional"
-	"github.com/phrase/phrase-go"
+	"github.com/phrase/phrase-go/v2"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
--- clients/go/test/api_uploads_test.go
+++ clients/go/test/api_uploads_test.go
@@ -17,34 +17,52 @@ import (
 	"testing"
 
 	"github.com/antihax/optional"
-	"github.com/phrase/phrase-go"
+	"github.com/phrase/phrase-go/v2"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func Test_phrase_UploadsApiService(t *testing.T) {
-	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Send the mock response
-		response := `{"id": "1", "filename": "test.json", "format": "json", "state": "valid" }`
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-
-		w.Write([]byte(response))
-	}))
-
-	defer server.Close()
-
-	configuration := phrase.NewConfiguration()
-	configuration.BasePath = server.URL
-	apiClient := phrase.NewAPIClient(configuration)
-
 	t.Run("Test UploadsApiService UploadCreate", func(t *testing.T) {
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// assert request body for proper serialization of tested format_options
+			r.ParseMultipartForm(0)
+			assert.Equal(t, r.FormValue("format_options[translation_columns][en]"), "f")
+			assert.Equal(t, r.FormValue("format_options[translation_columns][de]"), "e")
+			assert.Equal(t, r.FormValue("format_options[top_level_key]"), "100")
+
+			// Send the mock response
+			response := `{"id": "1", "filename": "test.json", "format": "json", "state": "valid" }`
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+	
+			w.Write([]byte(response))
+		}))
+	
+		defer server.Close()
+	
+		configuration := phrase.NewConfiguration()
+		configuration.BasePath = server.URL
+		apiClient := phrase.NewAPIClient(configuration)
+
 		file, _ := os.Create("testfile.json")
 		fileFormat := optional.NewString("json")
 		fileObject := optional.NewInterface(file)
 		localeId := optional.NewString("99")
 
-		localVarOptionals := phrase.UploadCreateOpts{FileFormat: fileFormat, File: fileObject, LocaleId: localeId }
+		// setting format_options
+		formatOptions := make(map[string]interface{})
+		nestedOptions := make(map[string]interface{})
+
+		nestedOptions["en"] = "f"
+		nestedOptions["de"] = "e"
+
+		formatOptions["top_level_key"] = 100
+		formatOptions["translation_columns"] = nestedOptions
+
+		formatOptionsMap := optional.NewInterface(formatOptions)
+
+		localVarOptionals := phrase.UploadCreateOpts{FileFormat: fileFormat, File: fileObject, LocaleId: localeId, FormatOptions: formatOptionsMap }
 		resp, httpRes, err := apiClient.UploadsApi.UploadCreate(context.Background(), "project_id", &localVarOptionals)
 		requestUrl := httpRes.Request.URL
 
@@ -62,6 +80,21 @@ func Test_phrase_UploadsApiService(t *testing.T) {
 	})
 
 	t.Run("Test UploadsApiService UploadShow", func(t *testing.T) {
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// Send the mock response
+			response := `{"id": "1", "filename": "test.json", "format": "json", "state": "valid" }`
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+	
+			w.Write([]byte(response))
+		}))
+	
+		defer server.Close()
+	
+		configuration := phrase.NewConfiguration()
+		configuration.BasePath = server.URL
+		apiClient := phrase.NewAPIClient(configuration)
+		
 		localVarOptionals := phrase.UploadShowOpts{}
 		resp, httpRes, err := apiClient.UploadsApi.UploadShow(context.Background(), "project_id", "upload_id", &localVarOptionals)
 		requestUrl := httpRes.Request.URL
