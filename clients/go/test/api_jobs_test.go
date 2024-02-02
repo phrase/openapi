ce7cec6cd61ad907a06347346e8ce7bb59ba59af
Hassan Ahmed <hassan.ahmed@phrase.com>
2024-02-02 16:52:56 +0100

    Corrected Assertion

--- clients/go/test/api_jobs_test.go
+++ clients/go/test/api_jobs_test.go
@@ -11,6 +11,7 @@ package phrase
 
 import (
 	"context"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -24,9 +25,15 @@ func Test_phrase_JobsApiService(t *testing.T) {
 	t.Run("Test JobsApiService JobCreate", func(t *testing.T) {
 		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			// assert request body
-			r.ParseForm()
-			assert.Equal(t, r.FormValue("name"), "createjobactionb1")
-			assert.Equal(t, r.FormValue("due_date"), "")
+			var requestBody map[string]interface{}
+            decoder := json.NewDecoder(r.Body)
+            if err := decoder.Decode(&requestBody); err != nil {
+                t.Fatal(err)
+            }
+
+            // Access the values from the JSON payload
+            assert.Equal(t, requestBody["name"], "createjobactionb1")
+			assert.Equal(t, requestBody["due_date"], "")
 
 			// Send the mock response
 			response := `{"id": "1", "name": "createjobactionb1", "code": "123abc", "state": "draft", "user_id": "1" }`
