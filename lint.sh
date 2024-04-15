63ec1375439a519ecd3839974150b5fd119fdb06
Mladen Jablanovic <mladen.jablanovic@phrase.com>
2024-04-15 08:55:29 +0200

    ignore schema name warning

--- lint.sh
+++ lint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-output=$(npx openapi-generator-cli validate -i tmp/compiled.yaml | grep -v "inheritance without use of 'discriminator.propertyName'" | grep -v "No validation issues detected.")
+output=$(npx openapi-generator-cli validate -i tmp/compiled.yaml | grep -v "inheritance without use of 'discriminator.propertyName'" | grep -v "No validation issues detected." | grep -v "Failed to get the schema name")
 if echo "$output" | grep -q -E "WARN|ERROR"; then
     echo "$output"
     echo "Validation errors"
